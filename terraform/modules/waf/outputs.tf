207442c9083167478c02adf9f32954dc6d852661
Vamshi krrishna reddy Gudi <55616390+vamsi8394@users.noreply.github.com>
2025-12-16 21:52:06 -0500

    Delete terraform directory

--- terraform/modules/waf/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_acl_arn" {
-  description = "ARN of the WAF Web ACL"
-  value       = aws_wafv2_web_acl.this.arn
-}
-
-output "web_acl_id" {
-  description = "ID of the WAF Web ACL"
-  value       = aws_wafv2_web_acl.this.id
-}
